6be91414639e772fd931a63f33e814946f5de1c5
Wei Huan Chen <wei.chen@vtex.com.br>
2022-04-06 13:24:48 -0400

    [FEDEXSHIP-16] added toast for successful/faulty saves

---
+++ react/components/Configurations.tsx
@@ -0,0 +1,97 @@
+import type { FC } from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+  PageContent,
+  Input,
+  InputPassword,
+  Heading,
+  Toggle,
+  Button,
+  useToast
+} from '@vtex/admin-ui'
+import { useQuery, useMutation } from 'react-apollo'
+import AppSettings from '../queries/getAppSettings.gql'
+import SaveAppSetting from '../mutations/saveAppSetting.gql'
+
+const Configurations: FC = () => {
+  const { data } = useQuery(AppSettings, {
+    ssr: false,
+  })
+  
+  const [state, setState] = useState<any>({
+    clientDetailMeterNumber: '',
+    clientDetailAccountNumber: '',
+    userCredentialKey: '',
+    userCredentialPassword: '',
+    isLive: false
+  })
+
+  const [saveAppSetting] = useMutation(SaveAppSetting);
+
+  useEffect(() => {
+    if (!data?.getAppSettings) return
+
+    const getAppSettings = data.getAppSettings
+    setState({...getAppSettings})
+  }, [data])
+
+  const showToast = useToast()
+
+  const { clientDetailMeterNumber, clientDetailAccountNumber, userCredentialKey, userCredentialPassword, isLive } = state
+
+  const handleSave = () => {
+    saveAppSetting({
+      variables: {
+        appSetting: {
+          userCredentialKey,
+          userCredentialPassword,
+          parentCredentialKey: '',
+          parentCredentialPassword: '',
+          clientDetailMeterNumber,
+          clientDetailAccountNumber,
+          isLive
+        }
+      }
+    }).then((result: any) => {
+      let message = 'Settings Saved'
+      if (!result?.data?.saveAppSetting) {
+        message = 'Error. Fail to Save'
+      }
+      showToast({
+        message
+      })
+    })
+  }
+
+
+  return (
+    <PageContent>
+      <div className="pv6">
+        <Heading>Settings</Heading>
+        <Input id="meter" label="Meter Number" value={clientDetailMeterNumber} 
+          onChange={(e) => setState({...state, clientDetailMeterNumber: e.target.value})}
+          />
+        <Input id="accountNumber" label="Account Number" value={clientDetailAccountNumber}
+          onChange={(e) => setState({...state, clientDetailAccountNumber: e.target.value})}
+          />
+        <InputPassword id="credentialKey" label="Credential Key" value={userCredentialKey}
+          onChange={(e) => setState({...state, userCredentialKey: e.target.value})}
+          />
+        <InputPassword id="credentialPwd" label="Credential Password" value={userCredentialPassword}
+          onChange={(e) => setState({...state, userCredentialPassword: e.target.value})}
+        />
+        <div>
+          Is Live: <Toggle
+            aria-label="label"
+            checked={isLive}
+            onChange={() => setState({...state, isLive: !isLive})}
+          />
+        </div>
+        <Button variant="primary" onClick={() => handleSave()}>Save</Button>
+      </div>
+    </PageContent>
+    )
+    
+}
+
+export default Configurations
